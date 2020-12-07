39f59a25d56fe81950e1278d2a655e449fd5300b
Darien H <darien.hillebrand@gmail.com>
2020-12-07 21:29:39 +0200

    Implement special upgrades for things that aren't infantry

--- app/CivData.ts
+++ app/CivData.ts
@@ -42,7 +42,7 @@ class CivData {
                     infantry: [],
                     specificUnits: [
                         { name: "Bagains", atk: 5, rof: 0, hp: 0, ma: 1, pa: 1, units: [UnitId.twoHandedSwordsman] },
-                        { name: "Stirrups", atk: 0, rof: 33, hp: 0, ma: 0, pa: 0, units: [UnitId.eKonnik, UnitId.cavalier] }
+                        { name: "Stirrups", atk: 0, rof: 25, hp: 0, ma: 0, pa: 0, units: [UnitId.eKonnik, UnitId.cavalier] }
                     ]
                 }
             ),
@@ -72,6 +72,9 @@ class CivData {
                 {
                     infantry: [
                         { name: "Civ bonus", atk: 3, rof: 0, hp: 0, ma: 0, pa: 0 }
+                    ],
+                    specificUnits: [
+                        { name: "Howdah", atk: 0, rof: 0, hp: 0, ma: 1, pa: 1, units: [UnitId.eBattleElephant] }
                     ]
                 }
             ),
@@ -150,11 +153,7 @@ class CivData {
             ),
             new Civ(24, "Indians", "Indian",
                 "https://vignette.wikia.nocookie.net/ageofempires/images/8/8b/CivIcon-Indians.png/revision/latest?cb=20191107173239",
-<<<<<<< HEAD
-                [units.champion, units.condottiero, units.halbardier, units.hussar, units.imperialCamel],
-=======
                 [units.champion, units.condottiero, units.halbardier, units.hussar, units.imperialCamel, units.knight, units.villager],
->>>>>>> add-villager
                 [upgrades.forging, upgrades.ironCasting, upgrades.blastFurnace],
                 [upgrades.scaleMailArmor, upgrades.chainMailArmor],
                 [upgrades.bloodlines, upgrades.scaleBardingArmor, upgrades.chainBardingArmor],
@@ -186,7 +185,7 @@ class CivData {
                 [upgrades.bloodlines, upgrades.scaleBardingArmor, upgrades.chainBardingArmor],
                 {
                     infantry: [
-                        { name: "Civ bonus", atk: 0, rof: 33, hp: 0, ma: 0, pa: 0 }
+                        { name: "Civ bonus", atk: 0, rof: 25, hp: 0, ma: 0, pa: 0 }
                     ]
                 }
             ),
@@ -358,7 +357,12 @@ class CivData {
                 [upgrades.forging, upgrades.ironCasting],
                 [upgrades.scaleMailArmor, upgrades.chainMailArmor, upgrades.plateMailArmor],
                 [upgrades.bloodlines, upgrades.scaleBardingArmor, upgrades.chainBardingArmor, upgrades.plateBardingArmor],
-                {infantry: []}
+                {
+                    infantry: [],
+                    specificUnits: [
+                        { name: "Chatras", atk: 0, hp: 16.67, rof: 0, ma: 0, pa: 0, units:[UnitId.eBattleElephant] }
+                    ]
+                }
             ),
             new Civ(44, "Vikings", "Viking",
                 "https://vignette.wikia.nocookie.net/ageofempires/images/c/c9/CivIcon-Vikings.png/revision/latest?cb=20191107173410",
--- app/CivUnit.ts
+++ app/CivUnit.ts
@@ -14,26 +14,23 @@ class CivUnit {
 
         // HP modification
         const hpUpgrades = civ.totalHPUpgrade(unit.type)
-        const hpSpecial = civ.totalSpecialHPUpgrade()
+        const hpSpecial = civ.totalSpecialHPUpgrade(unit.type, unit.id)
         const hpTotal = unit.hp + (this.unit.hp * (hpSpecial/100)) + hpUpgrades
 
         // ATK modification
         const atkUpgrades = civ.totalMeleeAtkUpgrade()
-        const atkSpecial = civ.totalSpecialAtkUpgrade()
+        const atkSpecial = civ.totalSpecialAtkUpgrade(unit.type, unit.id)
         const atkTotal = unit.atk + atkUpgrades + atkSpecial
 
         // ROF modification
-        var rofSpecial = 0
-        civ.special.infantry.forEach(function(upgrade) {
-            rofSpecial -= upgrade.rof
-        });
-        const rofTotal = unit.rof + (rofSpecial/100)
+        var rofSpecial = civ.totalSpecialROFUpgrade(unit.type, unit.id)
+        const rofTotal = unit.rof * (1 - rofSpecial/100)
 
         // Armor modification
         const maUpgrades = civ.totalBlacksmithMAUpgrade(unit.type)
         const paUpgrades = civ.totalBlacksmithPAUpgrade(unit.type)
-        const maSpecial = civ.totalSpecialMAUpgrade(unit.id)
-        const paSpecial = civ.totalSpecialPAUpgrade(unit.id)
+        const maSpecial = civ.totalSpecialMAUpgrade(unit.type, unit.id)
+        const paSpecial = civ.totalSpecialPAUpgrade(unit.type, unit.id)
         const maTotal = unit.ma + maSpecial + maUpgrades
         const paTotal = unit.pa + paSpecial + paUpgrades
 
